579981a46e8fa4b2e8b6c8ffcfb8c163103c3c46
wayanadha <wayanadha@users.noreply.github.com>
2026-06-29 20:19:19 +0700

    Create Flask application base

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return {
+    return jsonify({
         "project": "MiniCrypto Blockchain",
         "status": "running"
-    }
+    })
+
+
+@app.route("/test")
+def test():
+    return jsonify({
+        "message": "Flask API working"
+    })
 
 
 if __name__ == "__main__":
