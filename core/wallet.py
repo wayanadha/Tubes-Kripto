f2c7ba1ffaf5db7a1d67e33656a7a99963e00e6e
wayanadha <wayanadha@users.noreply.github.com>
2026-06-30 15:41:22 +0700

    Implement wallet key generation

---
+++ core/wallet.py
@@ -0,0 +1,40 @@
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import serialization
+
+
+class Wallet:
+
+    def __init__(self):
+        self.private_key = self.generate_private_key()
+        self.public_key = self.generate_public_key()
+
+
+    def generate_private_key(self):
+        private_key = ec.generate_private_key(
+            ec.SECP256K1()
+        )
+
+        return private_key
+
+
+    def generate_public_key(self):
+        public_key = self.private_key.public_key()
+
+        return public_key
+
+
+    def get_private_key_pem(self):
+
+        return self.private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+        ).decode()
+
+
+    def get_public_key_pem(self):
+
+        return self.public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        ).decode()
