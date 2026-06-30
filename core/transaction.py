f4dcf97d66b20d8b0fc394b92e5c4c2adf5285c7
wayanadha <wayanadha@users.noreply.github.com>
2026-06-30 19:45:22 +0700

    Implement transaction signing and verification

--- core/transaction.py
+++ core/transaction.py
@@ -2,6 +2,11 @@ import uuid
 import time
 import json
 
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import serialization
+from cryptography.exceptions import InvalidSignature
+
 
 class Transaction:
 
@@ -24,6 +29,64 @@ class Transaction:
 
         self.signature = None
         self.public_key = None
+
+
+    def get_payload(self):
+
+        payload = {
+            "tx_id": self.tx_id,
+            "type": self.type,
+            "from": self.sender,
+            "to": self.receiver,
+            "amount": self.amount,
+            "nonce": self.nonce,
+            "timestamp": self.timestamp
+        }
+
+        return json.dumps(
+            payload,
+            sort_keys=True
+        )
+
+
+    def sign_transaction(self, wallet):
+
+        payload = self.get_payload().encode()
+
+        signature = wallet.private_key.sign(
+            payload,
+            ec.ECDSA(hashes.SHA256())
+        )
+
+
+        self.signature = signature.hex()
+
+        self.public_key = wallet.get_public_key_pem()
+
+
+    def verify_signature(self):
+
+        try:
+
+            public_key = serialization.load_pem_public_key(
+                self.public_key.encode()
+            )
+
+
+            public_key.verify(
+                bytes.fromhex(self.signature),
+                self.get_payload().encode(),
+                ec.ECDSA(hashes.SHA256())
+            )
+
+
+            return True
+
+
+        except InvalidSignature:
+
+            return False
+
 
 
     def to_dict(self):
--- test_transaction.py
+++ test_transaction.py
@@ -1,12 +1,23 @@
+from core.wallet import Wallet
 from core.transaction import Transaction
 
 
+wallet = Wallet()
+
+
 tx = Transaction(
-    sender="ADDR_BUYER",
+    sender=wallet.address,
     receiver="ADDR_SELLER",
     amount=100,
     nonce=1
 )
 
 
+tx.sign_transaction(wallet)
+
+
 print(tx.to_dict())
+
+
+print("\nSignature Status:")
+print(tx.verify_signature())
